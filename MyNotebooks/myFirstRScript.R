005986a8d39b573c7b5549eaf19906d6736bf303
sophshi05 <67606783+sophshi05@users.noreply.github.com>
2020-07-05 16:49:25 -0400

    Update myFirstRScript.R

--- MyNotebooks/myFirstRScript.R
+++ MyNotebooks/myFirstRScript.R
@@ -1,3 +1,9 @@
-a <- 1
+# Read Chicago COVID19 time series data into R as a data frame
+# https://raw.githubusercontent.com/menonpg/covid19_Chicago/master/timeSeries_COVID19_Chicago.csv
+myData <- read.csv("https://raw.githubusercontent.com/menonpg/covid19_Chicago/master/timeSeries_COVID19_Chicago.csv")
 
-print(a)
+# Convert the date column appropriately so R recognizes it as a Date
+myData$Date <- as.Date(myData$Date, format="%Y-%m-%d")
+
+# Summarize the columns
+print(summary(myData))
